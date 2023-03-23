85dc0de8471035f7654e5ae6706d0723e9201e3e
zjysteven <jingyang.zhang@duke.edu>
2023-03-22 21:38:46 -0400

    change the way of generating json files

--- utils/convert.csv.to.json.py
+++ utils/convert.csv.to.json.py
@@ -1,49 +1,44 @@
 import pandas as pd
 import json
 import os
+import numpy as np
 
-def csv_to_multiple_json_files(input_csv_file, output_path):
+
+def csv_to_multiple_json_files(input_csv_file, output_root):
     # Read the CSV file
     df = pd.read_csv(input_csv_file)
 
     # Extract the first float value from the strings in the "NearOOD AUROC" and "FarOOD AUROC" columns
-    df["In-Distribution Accuracy Mean"] = df["In-Distribution Accuracy"].str.extract(r"(\d+\.\d+)", expand=False)
-    df["NearOOD AUROC Mean"] = df["NearOOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
-    df["FarOOD AUROC Mean"] = df["FarOOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["ID Accuracy Mean"] = df["ID Accuracy"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["Near-OOD AUROC Mean"] = df["Near-OOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["Far-OOD AUROC Mean"] = df["Far-OOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
 
-    # Rank rows separately for different "Dataset" values
-    df["Rank"] = df.groupby("Dataset")["NearOOD AUROC Mean"].rank(ascending=False, method='first').astype(int)
+    # Set up saving directory
+    dataset = input_csv_file.split('/')[-1].split('.')[0]
+    saving_dir = os.path.join(output_root, dataset)
+    os.makedirs(saving_dir, exist_ok=True)
 
-    # Iterate through the DataFrame, creating a JSON file for each unique combination of 'Paper' and 'Alias' values
-    for _, group in df.groupby(["Dataset","Paper", "Alias"]):
-        # Get the 'Paper' and 'Alias' values and use them as the filename
-        paper = group.iloc[0]["Paper"]
-        alias = group.iloc[0]["Alias"]
-        datasets = group.iloc[0]["Dataset"]
-        output_json_file = f"{paper}_{alias}.json"
-        cur_output_path = f"{output_path}/{datasets}"
-        # Create the output directory if it doesn't exist
-        os.makedirs(cur_output_path, exist_ok=True)
-
-        # Join the output path with the output JSON file name
-        output_file_path = os.path.join(cur_output_path, output_json_file)
-
-        # Drop the 'Paper' and 'Alias' columns and convert the remaining DataFrame to a JSON object
-        # group = group.drop(["Paper", "Alias"], axis=1)
-        json_data = group.to_dict(orient="records")
+    for i in range(len(df)):
+        training = df.iloc[i]["Training"]
+        postproc = df.iloc[i]["Postprocessor"]
+        extra_desc = df.iloc[i]["Additional Description"]
+        output_filename = f"{training}_{postproc}"
+        if not np.isnan(extra_desc):
+            output_filename += f"_{extra_desc}"
+        output_filename += ".json"
+        json_data = df.iloc[i:i+1].to_dict("records")
 
         # Write the JSON object to a file, one record per line
-        with open(output_file_path, "w") as file:
+        with open(os.path.join(saving_dir, output_filename), "w") as file:
             for record in json_data:
                 file.write(json.dumps(record, indent=2) + "\n")
 
-        print(f"Created {output_file_path}")
+        print(f"Created {os.path.join(saving_dir, output_filename)}")
+
 
 if __name__ == "__main__":
-    input_csv_file = "input.csv"
-    output_path = "/home/hz271/Research/OpenOOD.github.io/model_info"
-    csv_to_multiple_json_files(input_csv_file, output_path)
+    file_id = "cifar10"
 
-# convert.csv.to.json
-# 1. 读取input.csv
-# 2. 根据不同的数据集，在model_info下建立文件夹，并根据nearOOD_auroc排序，在每个文件夹下放每一个row对应的JSON
+    input_csv_file = f"../results/{file_id}.csv"
+    output_root = "../model_info"
+    csv_to_multiple_json_files(input_csv_file, output_root)
