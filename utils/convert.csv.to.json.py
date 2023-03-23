0a0b99e27a8d45bcecd618c523915ee3e4c13754
zjysteven <jingyang.zhang@duke.edu>
2023-03-22 23:01:44 -0400

    multiple changes

--- utils/convert.csv.to.json.py
+++ utils/convert.csv.to.json.py
@@ -4,14 +4,22 @@ import os
 import numpy as np
 
 
+temp = pd.read_csv("./papers.csv")
+temp = temp.set_index("Alias")
+paper_dict = temp.to_dict("index")
+
+
 def csv_to_multiple_json_files(input_csv_file, output_root):
     # Read the CSV file
     df = pd.read_csv(input_csv_file)
 
     # Extract the first float value from the strings in the "NearOOD AUROC" and "FarOOD AUROC" columns
-    df["ID Accuracy Mean"] = df["ID Accuracy"].str.extract(r"(\d+\.\d+)", expand=False)
-    df["Near-OOD AUROC Mean"] = df["Near-OOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
-    df["Far-OOD AUROC Mean"] = df["Far-OOD AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["ID_Accuracy_Mean"] = df["ID_Accuracy"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["Near-OOD_AUROC_Mean"] = df["Near-OOD_AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
+    df["Far-OOD_AUROC_Mean"] = df["Far-OOD_AUROC"].str.extract(r"(\d+\.\d+)", expand=False)
+
+    # Rank methods according to NearOOD AUROC by default
+    df["Rank"] = df["Near-OOD_AUROC_Mean"].rank(ascending=False, method='first').astype(int)
 
     # Set up saving directory
     dataset = input_csv_file.split('/')[-1].split('.')[0]
@@ -21,12 +29,16 @@ def csv_to_multiple_json_files(input_csv_file, output_root):
     for i in range(len(df)):
         training = df.iloc[i]["Training"]
         postproc = df.iloc[i]["Postprocessor"]
-        extra_desc = df.iloc[i]["Additional Description"]
+        extra_desc = df.iloc[i]["Additional_Description"]
         output_filename = f"{training}_{postproc}"
         if not np.isnan(extra_desc):
             output_filename += f"_{extra_desc}"
         output_filename += ".json"
         json_data = df.iloc[i:i+1].to_dict("records")
+        json_data[0]["Training_Link"] = paper_dict[training]["Link"] if training in paper_dict else ""
+        json_data[0]["PP_Link"] = paper_dict[postproc]["Link"]
+        if np.isnan(extra_desc):
+            json_data[0]["Additional_Description"] = ""
 
         # Write the JSON object to a file, one record per line
         with open(os.path.join(saving_dir, output_filename), "w") as file:
